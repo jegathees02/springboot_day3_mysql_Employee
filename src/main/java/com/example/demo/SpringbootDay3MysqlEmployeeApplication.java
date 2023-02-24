51bff651d82a0a10de1105ff0b47874245ae9e70
JEGATHEESWARAN T <727721eucs048@skcet.ac.in>
2023-02-24 13:30:08 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/SpringbootDay3MysqlEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDay3MysqlEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDay3MysqlEmployeeApplication.class, args);
+	}
+
+}
